42cd6577c1b377ce991a1a969bd90dae41850c29
Boris Dalstein <dalboris@gmail.com>
2021-06-07 17:47:27 +0200

    Modify central widget and toolbar to test widgets during development

--- libs/vgc/widgets/toolbar.cpp
+++ libs/vgc/widgets/toolbar.cpp
@@ -26,6 +26,16 @@
 
 #include <vgc/widgets/toolbar.h>
 
+#include <QLineEdit>
+
+#include <vgc/ui/button.h>
+#include <vgc/ui/column.h>
+#include <vgc/ui/label.h>
+#include <vgc/ui/lineedit.h>
+#include <vgc/ui/row.h>
+
+#include <vgc/core/format.h>
+
 namespace vgc {
 namespace widgets {
 
@@ -46,6 +56,16 @@ Toolbar::Toolbar(QWidget* parent) :
     topMargin->setStyleSheet("background-color: none");
     addWidget(topMargin);
 
+    QWidget* lineEdit = new QLineEdit();
+    addWidget(lineEdit);
+
+    colorToolButton_ = new ColorToolButton();
+    colorToolButton_->setToolTip(tr("Current color (C)"));
+    colorToolButton_->setStatusTip(tr("Click to open the color selector"));
+    colorToolButton_->setIconSize(iconSize);
+    colorToolButton_->setMinimumSize(toolbarWidth, iconWidth);
+    colorToolButton_->updateIcon();
+
     colorToolButton_ = new ColorToolButton();
     colorToolButton_->setToolTip(tr("Current color (C)"));
     colorToolButton_->setStatusTip(tr("Click to open the color selector"));
@@ -58,16 +78,32 @@ Toolbar::Toolbar(QWidget* parent) :
     colorToolButtonAction_->setToolTip(tr("Color (C)"));
     colorToolButtonAction_->setStatusTip(tr("Click to open the color selector"));
     colorToolButtonAction_->setShortcut(QKeySequence(Qt::Key_C));
-    colorToolButtonAction_->setShortcutContext(Qt::ApplicationShortcut);
+    //colorToolButtonAction_->setShortcutContext(Qt::ApplicationShortcut);
+
+    ui::RowPtr row = ui::Row::create();
+    row->createChild<ui::Button>("Button 1");
+    row->createChild<ui::LineEdit>("LineEdit With Long Text");
+    addWidget(new UiWidget(row, this));
 
     auto colorPalettePtr = ui::ColorPalette::create();
     colorPalette_ = colorPalettePtr.get();
     colorPaletteq_ = new UiWidget(colorPalettePtr, this);
-    // Note: it would be nice to std::move() the shared ptr instead of the
-    // above copy. This requires implementing UiWidget(WidgetPtr&&),
-    // but probably not worth it as this is temporary code anyway.
-    colorPaletteq_->setMinimumSize(toolbarWidth, 300);
     addWidget(colorPaletteq_);
+
+    ui::RowPtr row2 = ui::Row::create();
+    row2->createChild<ui::Button>("Button 3");
+    row2->createChild<ui::Button>("Button 4");
+    addWidget(new UiWidget(row2, this));
+
+    /*
+    auto labelPtr = ui::Label::create("Hello!");
+    //auto labelPtr = ui::Label::create("Ленивый рыжий кот");
+    //auto labelPtr = ui::Label::create("كسول الزنجبيل القط");
+    //auto labelPtr = ui::Label::create("ff");
+    auto* labelq = new UiWidget(labelPtr, this);
+    labelq->setMinimumSize(toolbarWidth, 40);
+    addWidget(labelq);
+    */
 
     connect(colorToolButtonAction_, SIGNAL(triggered()), colorToolButton_, SLOT(click()));
     connect(colorToolButton_, &ColorToolButton::colorChanged, this, &Toolbar::onColorToolButtonColorChanged_);
